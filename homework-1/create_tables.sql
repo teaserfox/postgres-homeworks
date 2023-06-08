8ef6c2add472863ed4ad801395285f578bdbd5f6
Kokorina Tatyana <teaserfox@gmail.com>
2023-06-09 02:03:08 +0300

    'необходима доработка'

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+
+
+create table employees_data
+(
+    employees_id int primary key,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	title  varchar(100),
+	birth_date date,
+	notes text
+);
+
+
+create table customers_data
+(
+    customer_id varchar(50) primary key,
+	company_name varchar(100) not null,
+	contact_name varchar(100) not null
+);
+
+
+create table orders_data
+(
+    orders_id int primary key,
+	customer_id varchar(50) references customers_data(customers_id) not null,
+	employee_id int references employees_data(employees_id) not null,
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,46 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+
+def open_csv_file(path):
+    """Возвращает список словарей"""
+    with open(path, 'r', encoding='utf-8') as file:
+        file_reader = csv.reader(file, delimiter=",")
+        data = []
+        for line in file_reader:
+            data.append(line)
+
+    return data
+
+
+#  параметры подключения к базе данных
+params = psycopg2.connect(host="localhost", database="north", user="postgres", password="777777")
+try:
+    with params:
+        with params.cursor() as cur:
+
+            employees = open_csv_file('../homework-1/north_data/employees_data.csv')  # открытие файла employees_data
+
+            for i_employees in employees:  # заполнение данными таблицы employees_data
+                cur.execute('INSERT INTO employees_data (employee_id, first_name, last_name, title, birth_date, notes)'
+                            ' VALUES (%s, %s, %s, %s, %s, %s)',
+                            (i_employees[0], i_employees[1],
+                             i_employees[2], i_employees[3],
+                             i_employees[4], i_employees[5]))
+
+            customers = open_csv_file('../homework-1/north_data/customers_data.csv')  # открытие файла customers
+
+            for i_customers in customers:  # заполнение данными таблицы customers_data
+                cur.execute('insert into customers_data values (%s, %s, %s)',
+                            (i_customers[0], i_customers[1], i_customers[2]))
+
+            orders = open_csv_file('../homework-1/north_data/orders_data.csv')  # открытие файла orders_data
+
+            for i_orders in orders:  # заполнение данными таблицы orders_data
+                cur.execute('insert into orders values (%s, %s, %s, %s, %s)',
+                            (i_orders[0], i_orders[1], i_orders[2],
+                             i_orders[3], i_orders[4]))
+finally:
+    params.close()
